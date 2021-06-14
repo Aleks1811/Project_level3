b8d956cea16e0ed093c7d002031cf8443998085c
Aleks1811 <Aleksmladenov1811@gmail.com>
2021-06-14 12:28:16 +0300

    Making progress

---
+++ script.js
@@ -0,0 +1,56 @@
+var arr = [['R','G','G','B'],
+['B','O','G','G'],
+['P','O','B','P'],
+['Y','B','Y','O']];
+
+// let down = document.getElementsByTagName('button')[3];
+// //console.log(down);
+// down.addEventListener('click',myFunction);
+
+//console.log(arr);
+
+// result.innerText = arr;
+
+// function myFunction(cell,direction){
+
+//     if (direction === 'Left') {
+//        swap = cell;
+// 	   cell = cell -1;
+// 	   cell-1 = swap;
+// 	   //console.log(arr); 
+//     }
+	
+// 	let result = document.getElementById('result');
+// 	for (let i = 0; i < arr.length; i++) {
+// 	    for (let j = 0; j < arr[i].length; j++) {
+// 	      //console.log(arr[i][j]);
+// 	      //document.write(arr[i][j]);
+// 	      result.innerText = arr;
+
+// 	    }
+// 	}
+
+
+// }
+
+// myFunction(arr[1][1],Left);
+// console.log(arr);
+
+let array = [1,4,5,6,6,7];
+
+function find(){
+
+for (var i=0; i<array.length; i++) {
+
+var previous=array[i-1];
+var current=array[i];
+var next=array[i+1];
+
+  }
+}
+
+console.log(find(i = 2));
+
+ 
+
+
---
+++ test.js
@@ -0,0 +1,49 @@
+let down = document.getElementsByTagName('button')[3];
+
+down.addEventListener("click", function(){
+  // (A) DUMMY ARRAY
+  var arr = [['R','G','G','B'],
+['B','O','G','G'],
+['P','O','B','P'],
+['Y','B','Y','O']];
+
+  // (B) CREATE HTML TABLE OBJECT
+  var perrow = 1; // 2 CELLS PER ROW
+  var table = document.createElement("table");
+  table.setAttribute('id','table');
+  var row = table.insertRow();
+
+  // LOOP THROUGH ARRAY AND ADD TABLE CELLS
+  for (var i = 0; i < arr.length; i++) {
+    // ADD "BASIC" CELL
+    var cell = row.insertCell();
+    cell.innerHTML = arr[i];
+
+    // ATTACH A RUNNING NUMBER OR CUSTOM DATA
+    cell.dataset.id = i;
+ 
+    //ATTACH ONCLICK LISTENER IF REQUIRED
+    cell.addEventListener("click", function(){
+      console.log(this.dataset.id); 
+    });
+
+     // BREAK INTO NEXT ROW
+    var next = i + 1;
+    if (next%perrow==0 && next!=arr.length) {
+      row = table.insertRow();
+    }
+  }
+
+  // (C) ATTACH TABLE TO CONTAINER
+  document.getElementById("container").appendChild(table);
+
+let result = document.getElementById('result');
+
+let swap = arr[0][1];
+arr[0][1] = arr[1][1];
+arr[1][1] = swap;
+//result.innerHTML = arr;
+
+
+
+});
